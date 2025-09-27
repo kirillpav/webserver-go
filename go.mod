7c3f0e7824c5537c15194f55a29feb9fdbc3ddf5
kirill pavlov <2020kirillp@gmail.com>
2025-09-27 09:47:17 -0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module boot.kirill
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(ch)
+
+		str := ""
+
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+
+			if err != nil {
+				break
+			}
+
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+				ch <- str
+				str = ""
+			}
+
+			str += string(data)
+		}
+		if len(str) != 0 {
+			ch <- str
+			// continue
+		}
+	}()
+
+	return ch
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":42069")
+	if err != nil {
+		log.Fatal("Error occured: ", err)
+	}
+
+	defer listener.Close()
+
+	// infinite loop
+	for {
+
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal("Error occured: %s", err)
+		}
+		fmt.Printf("Connection established: %s", conn.RemoteAddr().String())
+
+		buffer := make([]byte, 8)
+
+		n, err := conn.Read(buffer)
+		if err != nil {
+			log.Fatal("err")
+		}
+
+		getLinesChannel(n)
+
+	}
+
+}
